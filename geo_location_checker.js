a7f7d39068c113b5873e7c3bc5ca7edd6cdcd6a6
huuuuuuuuh <119647207+huuuuuuuuh@users.noreply.github.com>
2024-03-16 14:05:13 +0800

    geo_location_checker.js

--- geo_location_checker.js
+++ geo_location_checker.js
@@ -9,16 +9,6 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function City_ValidCheck(para) {
-  if(para) {
-  return para
-  } else
-  {
-  return city0
-//emojis[getRandomInt(emojis.length)]
-  }
-}
-
 function ISP_ValidCheck(para) {
   if(para) {
   return para
