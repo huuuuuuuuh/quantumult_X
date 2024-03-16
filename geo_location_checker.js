efb9dc0d59ec5e8b7303087ae9085ecf62f7e874
huuuuuuuuh <119647207+huuuuuuuuh@users.noreply.github.com>
2024-03-16 14:09:54 +0800

    geo_location_checker.js

--- geo_location_checker.js
+++ geo_location_checker.js
@@ -9,15 +9,6 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function ISP_ValidCheck(para) {
-  if(para) {
-  return para
-  } else
-  {
-  return isp0
-//emojis[getRandomInt(emojis.length)]
-  }
-}
 
 function Area_check(para) {
   if(para=="中华民国"){
@@ -33,7 +24,8 @@ var flags = new Map([["AC","🇦🇨"],["AD","🇦🇩"],["AE","🇦🇪"],["AF"
 var body = $response.body;
 var obj = JSON.parse(body);
 var title =flags.get(obj['countryCode']) + ' '+ Area_check(obj['country']);
-var subtitle = ISP_ValidCheck(obj['org']);
+var subtitle = obj['city'] + ' ' + obj['isp'];
 var ip = obj['query'];
-var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
+
 $done({title, subtitle, ip, description});
